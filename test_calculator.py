54dad7f7bde00b611e85f0eff480f76b4652c0d4
Lars B. <126595430+Driemtax@users.noreply.github.com>
2026-02-03 17:36:28 +0000

    modified:   test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,7 @@
 import calculator
 print(f"\n--- DEBUG: Starting test_calculator.py")
 def test_add():
-assert calculator.add(2, 3) == 5
-assert calculator.add(-1, 1) == 0
+    assert calculator.add(2, 3) == 5
+    assert calculator.add(-1, 1) == 0
 def test_subtract():
-assert calculator.subtract(5, 3) == 2
+    assert calculator.subtract(5, 3) == 2
